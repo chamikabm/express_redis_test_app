c0192191fe379ccba81b52f0ab3989f1395bc4f7
chamikab <kasun.chamika@gmail.com>
2018-10-06 00:16:54 +0530

    Added changes for modifying mongoose exec function prototype.

--- services/cache.js
+++ services/cache.js
@@ -3,6 +3,14 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
   console.log('Im about to run a query.');
+  console.log(this.getQuery());
+  console.log(this.mongooseCollection.name);
+
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+
+  console.log(key);
 
   return exec.apply(this, arguments);
 };
