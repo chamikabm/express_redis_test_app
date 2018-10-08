91e5f83d28bac3410122791eb20f25ec113a2bc3
chamikab <kasun.chamika@gmail.com>
2018-10-08 23:27:41 +0530

    Test case added for oauth.

--- tests/header.test.js
+++ tests/header.test.js
@@ -19,3 +19,9 @@ test('The header has the correct text.', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
+
+test('Clicking login starts oauth flow.', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch('/accounts\.google\.com/');
+});
