4fd70c4c369da6572a5af7b0857bc4ea9d589190
chamikab <kasun.chamika@gmail.com>
2018-10-14 13:13:26 +0530

    Added more test for the blog tests.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,9 +11,27 @@ afterEach(async () => {
   await page.close();
 });
 
-test('When logged in, should see blog create form.', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
-  const label = await page.getContentsOf('form label');
-  expect(label).toEqual('Blog Title');
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test('Should see blog create form.', async () => {
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+    test('The form shows error messages.', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,6 @@
 /*
 jest.setTimeout - Time to wait before failing a test case. Some times async await takes more time to complete a test
-case as it sometimes takes time to load web pages etc. By default it has only 5 seconds to complete a test case.
+case as it sometimes takes time to load web pages etc. By default it has only 5 seconds (5000ms) to complete a test case.
  */
 jest.setTimeout(30000);
 
