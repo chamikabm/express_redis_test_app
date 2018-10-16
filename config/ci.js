111c41e95a591cbbfd092108ccb38a97dcb4d75a
chamikab <kasun.chamika@gmail.com>
2018-10-16 22:22:09 +0530

    Added ci js files for CI related app config details.

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://chamika:cham123@ds221003.mlab.com:21003/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27917/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
 };
