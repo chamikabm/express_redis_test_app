4fe9a712e99b3fb21294cd08ce06300a2db8a095
chamikab <kasun.chamika@gmail.com>
2018-10-20 22:13:40 +0530

    Added changes for adding a input slector for image files.

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,10 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+
+  state = {
+    file: null,
+  };
   renderFields() {
     const { formValues } = this.props;
 
@@ -47,12 +51,23 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({file: event.target.files});
+    console.log("File 2 : " + event.target.files);
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
 
+        <h5>Add an Image</h5>
+        <input
+          accept="image/*"
+          onChange={this.onFileChange.bind(this)}
+          type="file"
+        />
         {this.renderButtons()}
       </form>
     );
